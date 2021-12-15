d4b0e42db593171a1a53e77d6c1aba5e20de46e5
ibifuro odu <oduibifuro@gmail.com>
2021-12-15 10:21:27 +0100

    search for words vertically and horizsontally

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,42 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+
+const wordSearch = (letters, word) => {
+  let count = 0;
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      count += 1;
     }
+  }
+
+  const outerColumn = [];
+  for (let i = 0; i < letters.length; i++) {
+    const column = [];
+    for (let j = 0; j < letters.length; j++) {
+      column.push(letters[j][i]);
+    }
+    outerColumn.push(column.join(''));
+  }
+
+  if (outerColumn.includes(word)) {
+    count += 1;
+  }
+
+  if (count > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-module.exports = wordSearch
+const result = wordSearch([
+  ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+  ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+  ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+  ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+  ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+  ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+  ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+  ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+  ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+], 'FRANK')
+module.exports = wordSearch;
